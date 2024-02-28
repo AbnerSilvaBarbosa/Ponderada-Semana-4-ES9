1de8cb81dbf4695b73d376cfe854b92f739ed0e3
AbnerSilvaBarbosa <abner.barbosa@sou.inteli.edu.br>
2024-02-28 08:34:48 -0300

    feat: add form dynamic in this project
    This is a lesson for Inteli, so i just see the tutorial in angular.io and write the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+
+import { DynamicFormComponent } from './components/dynamic-form.component/dynamic-form.component.component';
+
+import { QuestionService } from './components/question.service';
+import { QuestionBase } from './components/question-base';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-root',
+  template: `
+    <div>
+      <h2>Job Application for Heroes</h2>
+      <app-dynamic-form [questions]="questions$ | async"></app-dynamic-form>
+    </div>
+  `,
+  providers: [QuestionService],
+  imports: [AsyncPipe, DynamicFormComponent],
 })
 export class AppComponent {
-  title = 'SPA_angular_ponderada_ES9';
+  questions$: Observable<QuestionBase<any>[]>;
+
+  constructor(service: QuestionService) {
+    this.questions$ = service.getQuestions();
+  }
 }
---
+++ src/app/components/dynamic-form-question.component/dynamic-form-question.component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DynamicFormQuestionComponentComponent } from './dynamic-form-question.component.component';
+
+describe('DynamicFormQuestionComponentComponent', () => {
+  let component: DynamicFormQuestionComponentComponent;
+  let fixture: ComponentFixture<DynamicFormQuestionComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DynamicFormQuestionComponentComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DynamicFormQuestionComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
